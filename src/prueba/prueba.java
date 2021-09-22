d5fc8b0ccad1870f04a30e35f8cded5c9e75f8b3
j.linares <j.linares@IT272.indizen.com.es>
2021-09-22 20:59:43 +0200

    Prueba commit

--- src/prueba/prueba.java
+++ src/prueba/prueba.java
@@ -4,6 +4,9 @@ public class prueba {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("sdfsdfsd");
+		System.out.println("asdfasdf");
+		
+		int r = 0;
 
 	}
 	
